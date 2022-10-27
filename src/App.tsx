2cc191347e26095706761ce57df266712099ee4b
duygueroglu <duygu-1999-@hotmail.com>
2022-10-27 17:38:12 +0300

    store page updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import {Container} from "react-bootstrap"
 import {Home} from "./pages/Home"
 import {Store} from "./pages/Store"
 import {About} from "./pages/About"
+import {Navbar} from "./components/Navbar"
 function App() {
   return (
-  <Container>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/store" element={<Store/>}/>
-      <Route path="/about" element={<About/>}/>
-    </Routes>
-    </Container>
+    <>
+      <Navbar/>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/store" element={<Store/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </Container>
+    </>
   )
 }
 
